27f437004b2b44a88fa40fbbf50ab12583266ebd
Víctor Cândido <victorcandido@MacBook-Air-de-Victor.local>
2020-04-02 19:15:53 -0300

    Config de providers

--- app/js/controllers/listaTelefonicaCtrl.js
+++ app/js/controllers/listaTelefonicaCtrl.js
@@ -1,4 +1,5 @@
 angular.module('listaTelefonica').controller('listaTelefonicaCtrl', function($scope, contatosAPI, operadorasAPI, serialGenerator) {
+    console.log(serialGenerator.generate())
     $scope.app = "Lista Telefonica"
     $scope.contatos = []
 
